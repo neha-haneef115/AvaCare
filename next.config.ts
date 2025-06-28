5c08773a02651ed5a1057087a3dd356fe0ff08a3
Neha _Haneef <160058788+neha-haneef115@users.noreply.github.com>
2025-06-28 13:29:42 +0500

    Fix deployment errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   experimental: {
-    serverComponentsExternalPackages: ['mongodb']
+  experimental: {
+    serverExternalPackages: ['mongodb']
   }
 };
 
